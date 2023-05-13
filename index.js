8aca136f9b835972e21eb11714a8edc9a84a3299
neethupath <pathnee222gmail.com>
2023-05-13 02:14:56 -0400

    name

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+
+
+class Cat {
+    //...
+constructor(name,sex)
+{
+    this.name=name;
+    this.sex=sex;
+}
+     speak()
+    {
+
+return this.name + " says meow!";
+    }
+  }
+
+  class Dog {
+    constructor(name,sex)
+    {
+        this.name=name;
+        this.sex=sex;
+    }
+    //...
+    speak()
+    {
+
+        return this.name + " says woof!";
+    }
+  }
+
+  class Bird {
+    //...
+
+    constructor(name,sex)
+    {
+        this.name=name;
+        this.sex=sex;
+        this.speak = function () {
+            if (this.sex === "male") {
+              return `It's me! ${this.name}, the parrot!`;
+            } else {
+              return `${this.name} says squawk!`;
+            }
+          };
+    }
+  }
+  let  c=new Cat("Sasha",female);
+  let d=new Dog("Rufio",male);
+let b=new Bird("Pablo",male);
+let b1=new Bird("Mable",female);
